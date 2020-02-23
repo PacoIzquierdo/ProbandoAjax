9417c1bf61c9a2196154fa3447bdd0521eef3459
PacoIzquierdo <55537076+PacoIzquierdo@users.noreply.github.com>
2020-02-23 21:26:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -59,36 +59,48 @@ getJSON(urlCategorias).then(
 // El resto de funciones o lo que sea
 function resultadoCat(){
     
+  // Crear request
     var xhr = new XMLHttpRequest();
  
-   
+  // Recoger los valores para la busqueda
     var cat = document.getElementById('select-categorias').value;
     var cantidad = document.getElementById('select-cantidad').value;
     var tipo = document.getElementById("select-tipo").value
     
+  // Limpiar la parte de la página en la que saldrán
     document.getElementById('expositor').innerHTML = "";
-    xhr.open('GET','https://api.thecatapi.com/v1/images/search?api_key=271a4443-418d-4bf3-ae50-7b478da7f770&category_ids='+cat+'&limit='+cantidad+'&mime_types='+tipo, true);
-    
+
+    // Llamada a la api para obtener lo que sea, en este caso fotos de esas pequeñas y adorables bolas de pelo con cuchillos en las patas
+    xhr.open('GET','https://api.thecatapi.com/v1/images/search?api_key=271a4443-418d-4bf3-ae50-7b478da7f770&category_ids='+cat+'&limit='+cantidad+'&mime_types='+tipo, true);    
     xhr.send();
     
+    // Una vez se haya realizado la llamada, cambiar la página
     xhr.onreadystatechange = function(){
         
+        //Comprobar que todo ha ido bien
         if(xhr.readyState == 4 && xhr.status == 200){
             
+          // Crear string para añadir las cosas después a la zona en la que se mostrará
             var img = "";
+
+            // Interpretar los datos obtenidos
             var datos = JSON.parse(xhr.responseText);
             
+
+            // Bucle porque queremos más de una foto, en caso de que fuese solo una, el for es innecesario, pero no falla
             for(let c = 0 ; c < cantidad ; c++){
                 
                 img += "<img class='col-sm-10 col-md-4 col-lg-3' style='height: 20vh;' src='"+datos[c]["url"]+"'>";
                   
             }           
 
+            // Añadir todo a la página para visualizarlo
             document.getElementById('expositor').innerHTML += img;
             
         }
         
         else {
+          // JA!JA! Ahora puedes ir a la esquina a llorar
             console.log("Ha habido fallos");
         }
         
